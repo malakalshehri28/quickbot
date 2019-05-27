82054b0328f776629bd4db8f9d98798ce322119a
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-27 23:44:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-
+});
 
 
 const invites = {};
